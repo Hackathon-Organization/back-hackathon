1c9b6c1ad82751960d2100ee29daa0beb19756c3
alujuan <juanpeterssonb@gmail.com>
2023-11-25 07:01:27 -0300

    crud - tecn

--- modelagem/models/projetos.py
+++ modelagem/models/projetos.py
@@ -10,7 +10,7 @@ class Projetos(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.CharField(max_length=50)
     pontuacao = models.DecimalField(max_digits=10, decimal_places=2, verbose_name = "pontuação")
-    tecnologia = models.ForeignKey(Tecnologias, related_name="Projetos", on_delete=models.CASCADE, null=True, default=None)
+    #tecnologia = models.ForeignKey(Tecnologias, related_name="Projetos", on_delete=models.CASCADE, null=True, default=None)
     
     def __str__(self):
         return f"({self.nome}) - {self.pontuacao}"
